de8184ce73328c77eee824adf57dd05434dc5498
fire015 <emailfire@gmail.com>
2017-01-19 16:31:07 +0000

    Initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,15 @@
+function mailemail() {
+var x="function f(x){var i,o=\"\",l=x.length;for(i=0;i<l;i+=2) {if(i+1<l)o+=" +
+"x.charAt(i+1);try{o+=x.charAt(i);}catch(e){}}return o;}f(\"ufcnitnof x({)av" +
+" r,i=o\\\"\\\"o,=l.xelgnhtl,o=;lhwli(e.xhcraoCedtAl(1/)3=!84{)rt{y+xx=l;=+;" +
+"lc}tahce({)}}of(r=i-l;1>i0=i;--{)+ox=c.ahAr(t)i};erutnro s.buts(r,0lo;)f}\\" +
+"\"(8)\\\\,@\\\"BJUQ4H02\\\\\\\\O[31\\\\04\\\\00\\\\\\\\0^00\\\\\\\\\\\\rC\\" +
+"\\37\\\\07\\\\03\\\\\\\\04\\\\04\\\\01\\\\\\\\05\\\\03\\\\02\\\\\\\\25\\\\0" +
+"0\\\\00\\\\\\\\22\\\\0r\\\\\\\\\\\\13\\\\04\\\\01\\\\\\\\\\\\n7\\\\03\\\\\\" +
+"\\8-)5,39)k37v01\\\\\\\\rp\\\\\\\\*m\\\"\\\\\\\\\\\\%9j?4k02\\\\\\\\3,+kl 4" +
+"2[QHP_HVO[^t_W@BX[F\\\\\\\\\\\\\\\\NB20\\\\0F\\\\\\\\\\\\\\\\\\\\OKID01\\\\" +
+"0~\\\\34\\\\0F\\\\lz<u&z0;zy`p`z>f`{ahi~lf\\\"\\\\f(;} ornture;}))++(y)^(iA" +
+"tdeCoarchx.e(odrChamCro.fngriSt+=;o27=1y%2;*=)yy)8+>((iif){++;i<l;i=0(ior;f" +
+"thnglex.l=\\\\,\\\\\\\"=\\\",o iar{vy)x,f(n ioctun\\\"f)\")"                 ;
+while(x=eval(x));
+}
